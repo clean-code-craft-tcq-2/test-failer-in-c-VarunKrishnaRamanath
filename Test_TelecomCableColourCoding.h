238bd034d7afc3fb5e0c20f6d600cb97c083e33d
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-01-26 13:59:18 +0530

    Delete Test_TelecomCableColourCoding.h

--- Test_TelecomCableColourCoding.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_TELECOMCABLECOLOURCODING_H_INCLUDED
-#define TEST_TELECOMCABLECOLOURCODING_H_INCLUDED
-
-#include "Core_TelecomCableColourCoding.h"
-
-extern void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor);
-extern void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber);
-
-#endif // TEST_TELECOMCABLECOLOURCODING_H_INCLUDED
